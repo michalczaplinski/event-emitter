7407bf22a04494f00016917a3fcfd914baf74670
Michal Czaplinski <mmczaplinski@gmail.com>
2019-01-10 20:52:05 -0500

    some initial code :)

--- src/event-emitter.ts
+++ src/event-emitter.ts
@@ -1,6 +1,37 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+// import "core-js/fn/array.find"
 
+type Subscriptions = { [key: string]: Function[] }
+
+export default class EventEmitter {
+  _subscriptions: Subscriptions = {};
+
+  on(channel: string, handler: (data: any) => void): this {
+    if (!this._subscriptions[channel]) {
+      this._subscriptions[channel] = [];
+    };
+    this._subscriptions[channel].push(handler);
+    return this;
+  }
+
+  off(channel: string, handler: Function) {
+    if (!this._subscriptions[channel]) {
+      throw new Error(`There is no channel called ${channel}`)
+    }
+    const currentHandler = this._subscriptions[channel].find(h => h === handler);
+    if (!currentHandler) {
+      throw new Error(`There is no handler ${handler.name}`)
+    }
+
+    currentHandler()
+
+  }
+
+  send(channel: string, data: any) {
+    if (!this._subscriptions[channel]) return;
+
+    this._subscriptions[channel].forEach(subscriber => {
+      subscriber(data);
+    });
+  }
 }
--- test/event-emitter.test.ts
+++ test/event-emitter.test.ts
@@ -1,14 +1,68 @@
-import DummyClass from "../src/event-emitter"
+import EventEmitter from "../src/event-emitter"
 
-/**
- * Dummy test
- */
 describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
+
+  it('Emits', done => {
+    const emitter = new EventEmitter();
+
+    emitter.on('start', (data) => {
+      expect(data).toBe('hello');
+      done();
+    })
+    emitter.send('start', 'hello');
   })
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  it('is called exactly 3 times', () => {
+
+    const emitter = new EventEmitter();
+
+    const spy = jest.fn();
+    emitter.on('start', spy);
+
+    emitter.send('start', 'hello');
+    emitter.send('start', 'hello');
+    emitter.send('start', 'hello');
+
+    expect(spy).toHaveBeenCalledTimes(3);
+    expect(spy).toHaveBeenNthCalledWith(1, 'hello');
+    expect(spy).toHaveBeenNthCalledWith(2, 'hello');
+    expect(spy).toHaveBeenNthCalledWith(3, 'hello');
+    expect(spy).toHaveReturnedTimes(3);
+
   })
+
+  it('Can use a listener with no data', () => {
+
+    const emitter = new EventEmitter();
+
+    const spy = jest.fn();
+    emitter.on('start', spy);
+
+    emitter.send('start', 'hello');
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveReturnedTimes(1);
+
+  })
+
+  it('Does not do anything if we try to send a message to an unregistered channel', () => {
+    const emitter = new EventEmitter();
+
+    const spy = jest.fn();
+
+    emitter.send('start', 'hello');
+
+    expect(emitter._subscriptions).toEqual({});
+  })
+
+  it('Can unregister listeners', () => {
+    const emitter = new EventEmitter();
+
+    const spy = jest.fn();
+
+    emitter.send('start', 'hello');
+
+    expect(emitter._subscriptions).toEqual({});
+  })
+
 })
